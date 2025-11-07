ac91f3605347ce43796c1c138f634fb76f6b8b05
114-AWAB <76482549+114-AWAB@users.noreply.github.com>
2025-11-07 08:29:29 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="phoneinfoga-pro",
+    version="2.0.0",
+    author="Security Researcher",
+    author_email="research@example.com",
+    description="أداة متقدمة لجمع معلومات الهواتف من المصادر المفتوحة",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "phoneinfoga-pro=main:main",
+        ],
+    },
+)
